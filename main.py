c8419c1e4edda0f2b2321b206f62f0492b55bc40
Jubril Issa <issa.jubril@andela.com>
2016-09-25 22:39:32 +0100

    Debug travis error

--- main.py
+++ main.py
@@ -10,8 +10,8 @@
 
 import urllib2
 import json
-from google.appengine.ext import vendor
-vendor.add('lib')
+# from google.appengine.ext import vendor
+# vendor.add('lib')
 
 from flask import Flask
 app = Flask(__name__)
